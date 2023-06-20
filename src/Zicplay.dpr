53813128a2ca73fd4a70c6e3247eca34cccff193
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-06-20 18:39:01 +0200

    added about box and music library units

--- src/Zicplay.dpr
+++ src/Zicplay.dpr
@@ -4,7 +4,10 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   fMain in 'fMain.pas' {Form1},
-  Zicplay.Types in 'Zicplay.Types.pas';
+  Zicplay.Types in 'Zicplay.Types.pas',
+  Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialog.pas',
+  Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
+  Gamolf.FMX.MusicLoop in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.FMX.MusicLoop.pas';
 
 {$R *.res}
 
