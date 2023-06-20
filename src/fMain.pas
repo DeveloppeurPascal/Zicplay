abcdbf44579ec0d1ff2b17c933ca17ef422f9645
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-06-20 18:39:26 +0200

    added the main menu and some other basic components to main form

--- src/fMain.pas
+++ src/fMain.pas
@@ -3,11 +3,29 @@ unit fMain;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation, FMX.Menus, Olf.FMX.AboutDialog;
 
 type
   TForm1 = class(TForm)
+    MainMenu1: TMainMenu;
+    StatusBar1: TStatusBar;
+    ToolBar1: TToolBar;
+    AboutDialog: TOlfAboutDialog;
+    PopupMenu1: TPopupMenu;
+    MacSystemMenu: TMenuItem;
+    mnuFile: TMenuItem;
+    mnuHelp: TMenuItem;
+    mnuAbout: TMenuItem;
+    mnuExit: TMenuItem;
+    mnuTools: TMenuItem;
+    mnuOptions: TMenuItem;
+    procedure FormCreate(Sender: TObject);
+    procedure mnuExitClick(Sender: TObject);
+    procedure mnuAboutClick(Sender: TObject);
+    procedure mnuOptionsClick(Sender: TObject);
   private
     { Déclarations privées }
   public
@@ -20,5 +38,43 @@ var
 implementation
 
 {$R *.fmx}
+
+uses Zicplay.Types;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+{$IF Defined(ANDROID) or Defined(IOS)}
+  MainMenu1.Visible := false;
+{$ELSEIF Defined(MACOS) and not Defined(IOS)}
+  mnuExit.Visible := false; // already exists for Mac
+  mnuFile.Visible := false; // empty => no display
+  mnuOptions.Parent := MacSystemMenu;
+  mnuOptions.text := 'Preferences'; // TODO : translate text
+  mnuTools.Visible := false; // empty => no display
+{$ELSE}
+  MacSystemMenu.Visible := false;
+{$ENDIF}
+  caption := AboutDialog.Titre + ' ' + AboutDialog.VersionNumero;
+{$IFDEF DEBUG}
+  caption := caption + ' [DEBUG MODE]';
+{$ENDIF}
+end;
+
+procedure TForm1.mnuAboutClick(Sender: TObject);
+begin
+  AboutDialog.Execute;
+end;
+
+procedure TForm1.mnuExitClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TForm1.mnuOptionsClick(Sender: TObject);
+begin
+  showmessage('No option dialog in this release.');
+  // TODO : à compléter
+{$MESSAGE warn 'todo'}
+end;
 
 end.
