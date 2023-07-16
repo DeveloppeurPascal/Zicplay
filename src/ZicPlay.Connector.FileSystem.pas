501d05bcf44ef5baa53a2e3991554b4f3bfd908e
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-07-16 16:45:48 +0200

    FileSystem connector now get ID3 tags from MP3 files (#19)

--- src/ZicPlay.Connector.FileSystem.pas
+++ src/ZicPlay.Connector.FileSystem.pas
@@ -34,7 +34,10 @@ uses
   System.SysUtils,
   System.IOUtils,
   System.Types,
-  ZicPlay.Connector.FileSystem.PlaylistSetupDialog;
+  ZicPlay.Connector.FileSystem.PlaylistSetupDialog,
+  FMX.Media,
+  ID3v1,
+  ID3v2;
 
 { TZicPlayConnectorFileSystem }
 
@@ -95,43 +98,105 @@ ASearchSubFolders: Boolean; ACallbackProc: TZicPlayGetPlaylistProc);
     end;
   end;
 
+  function GetNewSong(Playlist: TPlaylist; Title, Artist, Album, Year,
+    Genre: string; Duration: integer): tsong;
+  begin
+    // TODO : add "Language" field to TSong
+    // TODO : add "Url" field to TSong
+    // TODO : add "TrackNumber" field to TSong
+    // TODO : add "AlbumTracksCount" field to TSong
+    // TODO : add "Copyright" field to TSong
+    result := tsong.create(Playlist);
+    result.Title := Title;
+    result.Artist := Artist;
+    result.Album := Album;
+    try
+      if (Year.length = 4) then
+        result.PublishedDate := encodedate(Year.tointeger, 1, 1)
+      else if (Year.length = 010) then // hope its YYYY-MM-DD
+        result.PublishedDate := encodedate(Year.substring(0, 4).tointeger,
+          Year.substring(6, 2).tointeger, Year.substring(8, 2).tointeger)
+      else if (Year.length > 4) then
+        result.PublishedDate := encodedate(Year.substring(0, 4).tointeger, 1, 1)
+      else
+        result.PublishedDate := now;
+    except
+      // TODO : list all values in libraries or add a log somewhere to see how to handle it
+      result.PublishedDate := now;
+    end;
+    result.Category := Genre;
+    result.Duration := Duration;
+  end;
+
 var
   i: integer;
   Files: TStringDynArray;
-  Song: TSong;
+  Song: tsong;
   Playlist: TPlaylist;
+  ID3v1: TID3v1;
+  ID3v2: TID3V2;
+  MediaPlayer: TMediaPlayer;
 begin
-  Playlist := TPlaylist.create;
+  ID3v1 := TID3v1.create;
   try
-    Playlist.Connector := self;
+    ID3v2 := TID3V2.create;
+    try
+      MediaPlayer := TMediaPlayer.create(nil);
+      try
+        Playlist := TPlaylist.create;
+        try
+          Playlist.Connector := self;
 
-    setlength(Files, 0);
-    GetFilesFromFolder(ASearchFolder, ASearchSubFolders, Files);
+          setlength(Files, 0);
+          GetFilesFromFolder(ASearchFolder, ASearchSubFolders, Files);
 
-    for i := 0 to length(Files) - 1 do
-      if (tpath.GetExtension(Files[i]).ToLower = '.mp3') then
-      begin
-        // TODO : get ID3 tags from the MP3 file and fill the TSong properties
-        Song := TSong.create(Playlist);
-        Song.Title := tpath.GetFileNameWithoutExtension(Files[i]);
-        Song.Artist := 'artists ' + Song.Title.ToLower;
-        Song.Album := tpath.GetDirectoryName(ASearchFolder);
-        Song.Duration := random(60 * 5);
-        Song.PublishedDate := now;
-        Song.Category := 'mp3';
-        Song.Order := 0;
-        Song.UniqID := Files[i];
-        Song.FileName := Files[i];
-        Song.onGetFilename := nil;
+          // TODO : add WAV files (wav)
+          // TODO : add WMA files (Windows Media Player)
+          // TODO : add M4A files (QuickTime)
+          // TODO : add OGG files (OGG Vorbis)
+          // TODO : add MID files (midi)
+          // TODO : add MOD files (Module Tracker & co)
+          for i := 0 to length(Files) - 1 do
+            if (tpath.GetExtension(Files[i]).ToLower = '.mp3') then
+            begin
+              if ID3v2.ReadFromFile(Files[i]) and ID3v2.Exists then
+                // TODO : get song duration
+                Song := GetNewSong(Playlist, ID3v2.Title, ID3v2.Artist,
+                  ID3v2.Album, ID3v2.Year, ID3v2.Genre, 0)
+              else if ID3v1.ReadFromFile(Files[i]) and ID3v1.Exists then
+                // TODO : get song duration
+                Song := GetNewSong(Playlist, ID3v1.Title, ID3v1.Artist,
+                  ID3v1.Album, ID3v1.Year, ID3v1.Genre, 0)
+              else
+                Song := GetNewSong(Playlist,
+                  tpath.GetFileNameWithoutExtension(Files[i]), 'unknown',
+                  tpath.GetFileNameWithoutExtension(Files[i]),
+                  FormatDateTime('yyyy-mm-dd', now), 'none', 0);
+//              if (Song.Duration = 0) then
+//              begin
+//                MediaPlayer.FileName := Files[i];
+//                Song.Duration := trunc(MediaPlayer.Duration / MediaTimeScale);
+//              end;
+              Song.Order := 0;
+              Song.UniqID := Files[i];
+              Song.FileName := Files[i];
+              Song.onGetFilename := nil;
 
-        Playlist.Add(Song);
+              Playlist.Add(Song);
+            end;
+        except
+          Playlist.Free;
+          raise;
+        end;
+      finally
+        MediaPlayer.Free;
       end;
-
-  except
-    Playlist.Free;
-    raise;
+    finally
+      ID3v2.Free;
+    end;
+  finally
+    ID3v1.Free;
   end;
-
   if assigned(ACallbackProc) then
     ACallbackProc(Playlist);
 end;
