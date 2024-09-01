16b17b23d2003e2257c9aed99b1299bb65a77a91
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-09-01 11:45:06 +0200

    added the ZicPlay logo to the new about box (#90)

---
+++ src/uDMAboutBoxLogoStorrage.pas
@@ -0,0 +1,59 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// FMX Tools Starter Kit
+///
+/// Copyright 2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://fmxtoolsstarterkit.developpeur-pascal.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/FMX-Tools-Starter-Kit
+///
+/// ***************************************************************************
+/// File last update : 2024-08-28T12:18:00.000+02:00
+/// Signature : fdfb9cbf0c63e73a5427aa4b35be5fdfd93aac37
+/// ***************************************************************************
+/// </summary>
+
+unit uDMAboutBoxLogoStorrage;
+
+interface
+
+{$MESSAGE WARN 'Save uDMAboutBoxLogoStorrage.pas in your project folder and to customize it in your project. Don''t change the template version if you want to be able to update it.'}
+// TODO : Save uDMAboutBoxLogoStorrage.pas in your project folder and to customize it in your project. Don't change the template version if you want to be able to update it.
+
+// TODO : when you'll have a logo or icon for your project, don't forget to replace default one by yours in the TImageList
+uses
+  System.SysUtils,
+  System.Classes,
+  System.ImageList,
+  FMX.ImgList;
+
+type
+  TdmAboutBoxLogo = class(TDataModule)
+    imgLogo: TImageList;
+  private
+  public
+  end;
+
+implementation
+
+{%CLASSGROUP 'FMX.Controls.TControl'}
+{$R *.dfm}
+
+end.
