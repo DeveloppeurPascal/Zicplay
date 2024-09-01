9da383e451aac391b659acdcf3df0a661ce5d542
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-09-01 12:33:31 +0200

    removed done TODOs

--- src/uDMAboutBoxLogoStorrage.pas
+++ src/uDMAboutBoxLogoStorrage.pas
@@ -34,10 +34,6 @@ unit uDMAboutBoxLogoStorrage;
 
 interface
 
-{$MESSAGE WARN 'Save uDMAboutBoxLogoStorrage.pas in your project folder and to customize it in your project. Don''t change the template version if you want to be able to update it.'}
-// TODO : Save uDMAboutBoxLogoStorrage.pas in your project folder and to customize it in your project. Don't change the template version if you want to be able to update it.
-
-// TODO : when you'll have a logo or icon for your project, don't forget to replace default one by yours in the TImageList
 uses
   System.SysUtils,
   System.Classes,
