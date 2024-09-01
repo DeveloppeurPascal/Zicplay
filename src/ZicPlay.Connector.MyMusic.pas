0c34442b0cad4bc940338a32743daebaec66af85
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-09-01 14:39:56 +0200

    removed the unused commented connector destruction

--- src/ZicPlay.Connector.MyMusic.pas
+++ src/ZicPlay.Connector.MyMusic.pas
@@ -64,9 +64,6 @@ TConnectorsList.current.Register(TZicPlayConnectorMyMusic.create);
 
 finalization
 
-// if assigned(TZicPlayConnectorMyMusic.current) then
-// TZicPlayConnectorMyMusic.current.Free;
-
 // TODO : find a way to free the instance if Delphi don't do it by itself
 
 end.
