f9d68b773bc4a2f1d2a29cea618dce7ca40d1c46
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-09-01 09:42:57 +0200

    updated paths to DeveloppeurPascal/Libraries units

--- src/Zicplay.dpr
+++ src/Zicplay.dpr
@@ -9,15 +9,15 @@ uses
   Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
   Gamolf.FMX.MusicLoop in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.FMX.MusicLoop.pas',
   uDMIcons in 'uDMIcons.pas' {DMIcons: TDataModule},
-  u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas',
+  u_urlOpen in '..\lib-externes\librairies\src\u_urlOpen.pas',
   ZicPlay.Connector.FileSystem in 'ZicPlay.Connector.FileSystem.pas',
   ZicPlay.Connector.FileSystem.PlaylistSetupDialog in 'ZicPlay.Connector.FileSystem.PlaylistSetupDialog.pas' {frmPlaylistSetupDialog},
   ZicPlay.Connector.MyMusic in 'ZicPlay.Connector.MyMusic.pas',
   uConfig in 'uConfig.pas',
   fPlaylist in 'fPlaylist.pas' {frmPlaylist},
   fSelectConnector in 'fSelectConnector.pas' {frmSelectConnector},
-  Olf.RTL.Streams in '..\lib-externes\librairies\Olf.RTL.Streams.pas',
-  Olf.RTL.DateAndTime in '..\lib-externes\librairies\Olf.RTL.DateAndTime.pas',
+  Olf.RTL.Streams in '..\lib-externes\librairies\src\Olf.RTL.Streams.pas',
+  Olf.RTL.DateAndTime in '..\lib-externes\librairies\src\Olf.RTL.DateAndTime.pas',
   ID3v1 in '..\lib-externes\Audio-Tools-Library\MP3-IDtags-Reader\ID3v1.pas',
   ID3v2 in '..\lib-externes\Audio-Tools-Library\MP3-IDtags-Reader\ID3v2.pas';
 
